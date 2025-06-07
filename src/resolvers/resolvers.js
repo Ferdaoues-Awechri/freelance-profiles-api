fd0e84d19885f13a66f6933310e3a245570759d5
Fer-012 <aouachriferdaoues@gmail.com>
2025-06-07 17:23:18 +0100

    add user mutation resolvers for create, update, delete, and skill/link management

--- src/resolvers/resolvers.js
+++ src/resolvers/resolvers.js
@@ -9,6 +9,46 @@ module.exports = {
     getUserByName: async (_, { name }) =>
       await User.findOne({ name: { $regex: name, $options: 'i' } }),
   },
+  Mutation: {
+    createUser: async (_, { name, email, bio, skills, links }) =>
+      await User.create({ name, email, bio, skills: skills || [], links: links || [] }),
+
+    updateUser: async (_, { id, ...updates }) =>
+      await User.findByIdAndUpdate(id, updates, { new: true }),
+
+    deleteUser: async (_, { id }) => {
+      await User.findByIdAndDelete(id);
+      return true;
+    },
+
+    addSkill: async (_, { userId, skill }) =>
+      await User.findByIdAndUpdate(
+        userId,
+        { $push: { skills: skill } },
+        { new: true }
+      ),
+
+    deleteSkill: async (_, { userId, skillName }) =>
+      await User.findByIdAndUpdate(
+        userId,
+        { $pull: { skills: { name: skillName } } },
+        { new: true }
+      ),
+
+    addLink: async (_, { userId, link }) =>
+      await User.findByIdAndUpdate(
+        userId,
+        { $push: { links: link } },
+        { new: true }
+      ),
+
+    deleteLink: async (_, { userId, url }) =>
+      await User.findByIdAndUpdate(
+        userId,
+        { $pull: { links: { url } } },
+        { new: true }
+      ),
+  },
 
 
 }
